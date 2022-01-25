98da7f513e529f76c9cf6c457d8c9248902a3913
armL4 <87949460+armL4@users.noreply.github.com>
2022-01-25 18:24:59 -0300

    mas arreglos

--- src/components/itemlistcontainer/itemDetail/ItemDetail.js
+++ src/components/itemlistcontainer/itemDetail/ItemDetail.js
@@ -1,47 +1,48 @@
 import React, { useState , useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { CartContext } from '../../Cart/cartContext'
+import {useCartContext} from '../../Cart/cartContext'
+
 import ItemCount from '../../Itemcount/Itemcount'
 
-import './ItemDetail.scss'
 
 const ItemDetail = ({productos}) => {
   
-    const [show, setshow] = useState(true)
-    
-    const {cartList, agregarAlCarrito} = useContext(CartContext)
- 
-     console.log(cartList)
- 
-    const onAdd = (cant) => {
-        console.log(cant)
-         agregarAlCarrito({...productos, cantidad:cant})
- 
-         setshow(false)
-        //console.log(`Usted ha comprado ${counter} productos de ${producto.name}`)
-        //alert(`Usted ha comprado ${counter} productos de ${producto.name}`)
-        
-    } 
- 
-     return (
-         <div className="ItemDetail">
-             <img src={productos.img} alt={productos.name}></img>
-             <h3>{productos.name}</h3>
-             <h4>{productos.year}</h4>
-             <h4>{productos.price}</h4>
-             <p>{productos.description}</p>
- 
-        
-         {show ? <ItemCount max={10} min={1} onAdd={onAdd} /> : 
-         <div>
-              <Link  to="/Cart"><button>Terminar la Compra</button></Link>
-              <Link to="/"><button>Seguir Comprando</button></Link>
-              </div> }
-             
-        
-        
-         </div>
-     )
- }
- 
- export default ItemDetail
+   const [show, setshow] = useState(true)
+   
+   const {cartList, agregarAlCarrito} = useCartContext()
+
+    console.log(cartList)
+
+   const onAdd = (cant) => {
+       console.log(cant)
+        agregarAlCarrito({...productos, cantidad:cant})
+
+        setshow(false)
+       //console.log(`Usted ha comprado ${counter} productos de ${producto.name}`)
+       //alert(`Usted ha comprado ${counter} productos de ${producto.name}`)
+       
+   } 
+
+    return (
+        <div className="ItemDetail">
+            <img src={productos.img} alt={productos.name}></img>
+            <h3>{productos.name}</h3>
+            <h4>{productos.year}</h4>
+            <h4>{productos.price}</h4>
+            <p>{productos.description}</p>
+            
+
+       
+        {show ? <ItemCount max={10} min={1} onAdd={onAdd} /> : 
+        <div>
+             <Link  to="/cart"><button>Ir al Carrito</button></Link>
+             <Link to="/"><button>Seguir Comprando</button></Link>
+             </div> }
+            
+       
+       
+        </div>
+    )
+}
+
+export default ItemDetail
