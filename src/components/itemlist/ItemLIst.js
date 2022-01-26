929552c6a6c0a2d617cb649630f8948f8f6f6577
armL4 <87949460+armL4@users.noreply.github.com>
2022-01-25 23:49:02 -0300

    cambios 2

--- src/components/itemlist/ItemLIst.js
+++ src/components/itemlist/ItemLIst.js
@@ -7,7 +7,13 @@ import Item from '../item/item.js'
 const ItemList = ({productos}) => {
     return (
         <div className="desayuno row">
-            { productos.map(productos => <Item key={productos.id} nombre ={productos.name}  img={productos.img} precio ={productos.price} year={productos.year} description={productos.description} /> )}
+            { productos.map(productos => 
+            <Item key={productos.id}
+                nombre ={productos.name} 
+                img={productos.img} 
+                precio ={productos.price} 
+                year={productos.year} 
+                description={productos.description} /> )}
         </div>
     )
 }
--- src/components/itemlistcontainer/itemDetail/ItemDetail.js
+++ src/components/itemlistcontainer/itemDetail/ItemDetail.js
@@ -1,11 +1,11 @@
 import React, { useState , useContext } from 'react'
 import { Link } from 'react-router-dom'
-import {useCartContext} from '../../Cart/cartContext'
+import {useCartContext} from '../../Cart/cartcontext'
 
 import ItemCount from '../../Itemcount/Itemcount'
+import './ItemDetail.scss'
 
-
-const ItemDetail = ({productos}) => {
+const ItemDetail = ({producto}) => {
   
    const [show, setshow] = useState(true)
    
@@ -15,7 +15,7 @@ const ItemDetail = ({productos}) => {
 
    const onAdd = (cant) => {
        console.log(cant)
-        agregarAlCarrito({...productos, cantidad:cant})
+        agregarAlCarrito({...producto, cantidad:cant})
 
         setshow(false)
        //console.log(`Usted ha comprado ${counter} productos de ${producto.name}`)
@@ -24,24 +24,42 @@ const ItemDetail = ({productos}) => {
    } 
 
     return (
-        <div className="ItemDetail">
-            <img src={productos.img} alt={productos.name}></img>
-            <h3>{productos.name}</h3>
-            <h4>{productos.year}</h4>
-            <h4>{productos.price}</h4>
-            <p>{productos.description}</p>
-            
+    <>
 
-       
-        {show ? <ItemCount max={10} min={1} onAdd={onAdd} /> : 
-        <div>
-             <Link  to="/cart"><button>Ir al Carrito</button></Link>
-             <Link to="/"><button>Seguir Comprando</button></Link>
-             </div> }
-            
-       
-       
+
+        <div className="Item-Detail-Grid">
+            <div className="Img-Detail">
+            <img src={producto.img} alt={producto.name}></img>
+            </div>
+
+            <div className="Info-Detail">
+                <h4>Precio : $ {producto.price}</h4>
+            </div>
+
+            <div className="Description-Detail">
+                <div className="nombreYear">
+                <h3>{producto.name}</h3>
+                <h4>Año : {producto.year}</h4>
+
+                </div>
+
+
+                <p>{producto.description}</p>
+            </div>
+        
+        <div className="Contador-Detail">
+            {show ? <ItemCount max={10} min={1} onAdd={onAdd} /> : 
+            <div>
+                <Link  to="/cart"><button>Ir al Carrito</button></Link>
+                <Link to="/"><button>Seguir Comprando</button></Link>
+                </div> }
+                
+        
+        
+            </div>
         </div>
+                
+    </>
     )
 }
 
